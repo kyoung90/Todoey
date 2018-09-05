f5f6be09eb9f96273579b9fe4573497253e2e064
Kenneth Young Castro <kenneth.young90@gmail.com>
2018-09-05 09:18:33 -0400

    Convert to MVC design pattern.

--- Todoey/Controllers/TodoListViewController.swift
+++ Todoey/Controllers/TodoListViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
     
-    var itemArray = ["Find Mike", "Buy eggos", "Destroy Demogorgon"]
+    var itemArray = [item]()
     
     let defaults = UserDefaults.standard
 
@@ -18,7 +18,21 @@ class TodoListViewController: UITableViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        if let item = defaults.array(forKey: "TodoListArray") as? [String]{
+        let newItem = item()
+        newItem.title = "Find Mike";
+        itemArray.append(newItem);
+        
+        let newItem2 = item()
+        newItem2.title = "Buy Eggos";
+        itemArray.append(newItem2);
+        
+        let newItem3 = item()
+        newItem3.title = "Destroy Demogorgon";
+        itemArray.append(newItem3);
+        
+
+        
+        if let item = defaults.array(forKey: "TodoListArray") as? [item]{
             itemArray = item
         }
         
@@ -33,7 +47,16 @@ class TodoListViewController: UITableViewController {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
         
-        cell.textLabel?.text = itemArray[indexPath.row]
+        cell.textLabel?.text = itemArray[indexPath.row].title
+        
+//        if (tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark){
+//            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+//        }
+//        else{
+//            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+//        }
+
+        cell.accessoryType = itemArray[indexPath.row].done == true ? .checkmark : .none
         
         return cell
     }
@@ -42,14 +65,26 @@ class TodoListViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
 //        print(itemArray[indexPath.row])
         
+            itemArray[indexPath.row].done = !itemArray[indexPath.row].done
         
+//        if (itemArray[indexPath.row].done){
+//            itemArray[indexPath.row].done = false;
+//            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+//        }
+//        else{
+//            itemArray[indexPath.row].done = true;
+//            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+//        }
+
         
-        if (tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark){
-            tableView.cellForRow(at: indexPath)?.accessoryType = .none
-        }
-        else{
-            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-        }
+//        if (tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark){
+//            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+//        }
+//        else{
+//            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+//        }
+        
+        tableView.reloadData()
         
         tableView.deselectRow(at: indexPath, animated: true)
         
@@ -65,11 +100,16 @@ class TodoListViewController: UITableViewController {
         
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
             //What will happen once the user clicks on the Add Item button on our UIAlert
-            self.itemArray.append(textField.text!)
+            
+            let newItem = item()
+            newItem.title = textField.text!;
+            
+            self.itemArray.append(newItem)
             
             self.defaults.set(self.itemArray, forKey: "TodoListArray")
             
             self.tableView.reloadData()
+            
         }
         
         alert.addTextField { (alertTextField) in
---
+++ Todoey/Data Model/item.swift
@@ -0,0 +1,14 @@
+//
+//  item.swift
+//  Todoey
+//
+//  Created by SkillsUSA on 8/30/18.
+//  Copyright © 2018 Kenneth Young. All rights reserved.
+//
+import Foundation
+
+class item{
+    var title : String = "";
+    var done : Bool = false;
+    
+}
